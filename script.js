5ca28ccb4a3cb5402a1df582b3b71dca5e295dc4
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-01-31 06:47:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function receber(){
+let nome = document.getElementById("numbercard").value
+document.getElementById("valor-numero").innerHTML = nome
+}
+
+
+
+function nomevalor(){
+    let nome = document.getElementById("seunome").value
+    document.getElementById("valor-nome").innerHTML = nome
+    }
+
+
+    function date(){
+        let nome = document.getElementById("datavalue").value
+        document.getElementById("valor-data").innerHTML = nome
+        }
+
+
+        
+    function date2(){
+        let nome = document.getElementById("data2value").value
+        document.getElementById("outra-data").innerHTML = nome
+        }
+        
+
+                
+    function cvcs(){
+        let nome = document.getElementById("cvc").value
+        document.getElementById("cvc-value").innerHTML = nome
+        }
